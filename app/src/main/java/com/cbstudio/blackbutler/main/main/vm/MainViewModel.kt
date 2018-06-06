5b44ac7f457540015ebc8dee66ff31d1aafc2c20
ImL1s <aa22396584@gmail.com>
2018-06-06 17:43:07 +0800

    refactor: some unused code

--- app/src/main/java/com/cbstudio/blackbutler/main/main/vm/MainViewModel.kt
+++ app/src/main/java/com/cbstudio/blackbutler/main/main/vm/MainViewModel.kt
@@ -1,8 +1,5 @@
 package com.cbstudio.blackbutler.main.main.vm
 
-import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.Transformations
-import com.cbstudio.blackbutler.extensions.onNext
 import com.cbstudio.blackbutler.main.base.vm.BaseViewModel
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.Subject
@@ -14,15 +11,7 @@ import io.reactivex.subjects.Subject
  */
 class MainViewModel : BaseViewModel() {
 
-    val textLiveData: MutableLiveData<String> = MutableLiveData()
     val startClickSubject: Subject<Unit> = BehaviorSubject.create()
     val stopClickSubject: Subject<Unit> = BehaviorSubject.create()
 
-    init {
-        textLiveData.value = "CLICK"
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-    }
 }
